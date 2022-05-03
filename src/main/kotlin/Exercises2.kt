747e48153f7f5b67ade6bc7bd21489672745871f
xuanniu <xuanniu@gmail.com>
2022-05-03 17:45:01 -0400

    init

--- src/main/kotlin/Exercises2.kt
+++ src/main/kotlin/Exercises2.kt
@@ -18,15 +18,49 @@ fun main(){
     println("Factorial of $m is $factorial")
 
     //Exercise 3
-
+    for (i in 1..10) {
+        println("10 * $i = ${i*10}")
+    }
 
     //Exercise 4
     var fib1 = 1
     var fib2 = 1
-    while (fib2 < 100) {
+    while (fib1 < 100) {
         var temp = fib2
         fib2 += fib1
         print("$fib1 ")
         fib1 = temp
     }
+
+    //Exercise 5
+    val numArry: IntArray = intArrayOf(1,2,3,6,4,2,0)
+    for (num in numArry) {
+        println(num)
+    }
+
+    //Exercise 6
+    val numArry2: DoubleArray = doubleArrayOf(23.4, -34.5, 50.0, 33.5, 55.5, 43.7, 5.7, -66.5)
+    var max = Double.MIN_VALUE;
+    for (num in numArry2) {
+        if (num > max) {
+            max = num;
+        }
+    }
+    println("Max value is $max")
+
+    //Exercise 7
+    val numArry3: DoubleArray = doubleArrayOf(45.3, 67.5, -45.6, 20.34, 33.0, 45.6)
+    var sum2 = 0.0
+    for (num in numArry3) {
+        sum2 += num
+    }
+    println("Average value of array is ${sum2/numArry3.size}")
+
+    //Exercise 8
+    for (i in 0..255) {
+        println("Ascii value of $i is ${i.toChar()}")
+    }
+
+
+
 }
